c956813fbda63463f2380f93cc05fb03378703b7
Michał Drabik <micdra7@gmail.com>
2019-10-27 19:20:38 +0100

    further config and initial styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 const App = () => {
     return (
@@ -10,6 +10,6 @@ const App = () => {
             </Router>
         </div>
     );
-}
+};
 
 export default App;
