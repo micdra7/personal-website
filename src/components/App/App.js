036e17f8bdd5bc7102323f619b27b4c590fbd942
Michal Drabik <micdra7@gmail.com>
2019-11-03 18:42:32 +0100

    Added CurrentInfo component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import FlipSection from '../FlipSection/FlipSection';
 import MainInfoSection from '../MainInfoSection/MainInfoSection';
+import CurrentInfo from '../CurrentInfo/CurrentInfo';
 
 const App = () => {
     return (
         <div className="app-wrapper">
             <FlipSection>
                 <Router>
-                    <Route exact path="/" render={() => <p>xd</p>} />
+                    <Route exact path="/" render={() => <CurrentInfo />} />
                 </Router>
             </FlipSection>
             
---
+++ src/components/CurrentInfo/CurrentInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CurrentInfo.scss';
+
+const CurrentInfo = () => {
+    return (
+        <div className="current-info">
+            <h3>Student at Silesian University of Technology</h3>
+            <h5>Faculty of Automatic Control, Electronics and Computer Science</h5>
+        </div>
+    );
+};
+
+export default CurrentInfo;
