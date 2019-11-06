7623f60965d341d02fa74a89e244a84413b6bbb8
Michal Drabik <micdra7@gmail.com>
2019-11-06 19:24:28 +0100

    minor adjustments

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,14 +7,14 @@ const Contact = () => {
     return (
         <div className="contact">
            <h1>
-               <a href="https://www.linkedin.com/in/micha%C5%82-drabik-5497b4191/">
+               <a href="https://www.linkedin.com/in/micha%C5%82-drabik-5497b4191/" rel="nofollow">
                    LinkedIn 
                    &nbsp;
                    <FontAwesomeIcon icon={faLinkedin} />
                </a>
            </h1>
            <h1>
-               <a href="https://github.com/micdra7">
+               <a href="https://github.com/micdra7" rel="nofollow">
                    GitHub 
                    &nbsp;
                    <FontAwesomeIcon icon={faGithub} />
