7bb5f14204df580919b02ccc227b706c09025189
Michal Drabik <micdra7@gmail.com>
2019-11-06 19:03:22 +0100

    feat: updated CurrentInfo

--- src/components/CurrentInfo/CurrentInfo.js
+++ src/components/CurrentInfo/CurrentInfo.js
@@ -4,8 +4,15 @@ import './CurrentInfo.scss';
 const CurrentInfo = () => {
     return (
         <div className="current-info">
-            <h3>Student at Silesian University of Technology</h3>
-            <h5>Faculty of Automatic Control, Electronics and Computer Science</h5>
+            <h3>About me</h3>
+            <p>
+                I am an aspiring fullstack developer with particular love towards C# (.NET Core) and Javascript (React).
+                Since I am constantly trying to improve and expand my skillset, I am open to any other tech stack.
+            </p>
+            <p>
+                I am currently studying at Silesian University of Technology, 
+                faculty of Automatic Control, Electronics and Computer Science.
+            </p>
         </div>
     );
 };
