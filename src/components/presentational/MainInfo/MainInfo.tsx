cd175800831551f4319d793d5ee708798949767e
Michal Drabik <micdra7@gmail.com>
2020-02-15 22:23:45 +0100

    feat: finished site for mobiles (vertical)

--- src/components/presentational/MainInfo/MainInfo.tsx
+++ src/components/presentational/MainInfo/MainInfo.tsx
@@ -3,14 +3,25 @@ import './MainInfo.scss';
 
 const MainInfo: React.FC = () => (
     <div className='main-info'>
+        <header>
+            <h1>Michal Drabik</h1>
+            <h5>Fullstack Web Developer</h5>
+        </header>
+
+        <main>
         <h3>About me</h3>
-        <p>I am an aspiring fullstack developer with particular love 
-            for C# (.NET Core) and Javascript (React). Since I am 
-            constantly trying to improve and expand my skillset, I am open 
-            to any other tech stack.</p>
-        <p>I am currently studying Computer Science at 
-            Silesian University of Technology, faculty of 
-            Automatic Control, Electronics and Computer Science.</p>
+            <p>
+                I am an aspiring fullstack developer with particular love
+                for C# (.NET Core) and Javascript (React). Since I am
+                constantly trying to improve and expand my skillset, I am open
+                to any other tech stack.
+            </p>
+            <p>
+                I am currently studying Computer Science at
+                Silesian University of Technology, faculty of
+                Automatic Control, Electronics and Computer Science.
+            </p>
+        </main>
     </div>
 );
 
