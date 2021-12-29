893f350f7e43902e70040e0f7ac6a55276126d06
Michal Drabik <micdra7@gmail.com>
2021-12-29 18:02:16 +0100

    feat: prepare basic menu burger

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,19 +1,32 @@
-import { Box, Flex, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Grid,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { ColorModeSwitcher } from './color-mode-switcher';
 import { LanguageSwitcher } from './language-switcher';
+import { MenuBurger } from './menu-burger';
 
 interface ILayoutProps {
   children: JSX.Element | JSX.Element[] | string;
 }
 
-export const Layout = ({ children }: ILayoutProps) => (
-  <Flex textAlign="center" fontSize="xl" justify="center">
-    <Grid minH="100vh" p={6} w="100%" maxW="1600px">
-      <Flex justify="end">
-        <LanguageSwitcher />
-        <ColorModeSwitcher />
-      </Flex>
-      <Box>{children}</Box>
-    </Grid>
-  </Flex>
-);
+export const Layout = ({ children }: ILayoutProps) => {
+  const defaultIsOpen = useBreakpointValue({ base: false, lg: true });
+  const { isOpen, onToggle } = useDisclosure({ defaultIsOpen });
+
+  return (
+    <Flex textAlign="center" fontSize="xl" justify="center">
+      <Grid minH="100vh" p={6} w="100%" maxW="1600px">
+        <Flex justify="end">
+          <LanguageSwitcher />
+          <ColorModeSwitcher />
+        </Flex>
+        <Box>{children}</Box>
+        <MenuBurger isOpen={isOpen} onToggle={onToggle} />
+      </Grid>
+    </Flex>
+  );
+};
---
+++ src/components/menu-burger.tsx
@@ -0,0 +1,24 @@
+import { Icon, IconButton } from '@chakra-ui/react';
+import { FaArrowRight, FaBars } from 'react-icons/fa';
+
+interface IMenuBurgerProps {
+  isOpen: boolean;
+  onToggle: () => void;
+}
+
+export const MenuBurger = ({ isOpen, onToggle }: IMenuBurgerProps) => (
+  <IconButton
+    display={['flex', null, null, 'none']}
+    justifyContent="center"
+    justifyItems="center"
+    alignItems="center"
+    pos="fixed"
+    top="50%"
+    right="6"
+    size="lg"
+    aria-label="toggle menu"
+    variant="ghost"
+    onClick={onToggle}
+    icon={<Icon boxSize="1.5em" as={isOpen ? FaArrowRight : FaBars} />}
+  />
+);
