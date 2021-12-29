bf4d820d310e78aef90a0a2205be95c2230ec95f
Michal Drabik <micdra7@gmail.com>
2021-12-29 17:09:18 +0100

    feat: add auto language detection

--- src/i18next/index.ts
+++ src/i18next/index.ts
@@ -2,21 +2,23 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { English } from './translations/english';
 import { Polish } from './translations/polish';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 export const initTranslations = () => {
-  i18next.use(initReactI18next).init({
-    resources: {
-      pl: {
-        translation: Polish,
+  i18next
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      resources: {
+        pl: {
+          translation: Polish,
+        },
+        en: {
+          translation: English,
+        },
       },
-      en: {
-        translation: English,
+      interpolation: {
+        escapeValue: false,
       },
-    },
-    lng: 'pl',
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+    });
 };
