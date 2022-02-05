68338343c4964e7491654aab95902d34d7798a22
Michal Drabik <micdra7@gmail.com>
2022-02-05 14:17:24 +0100

    chore: setup CRA with basic dependencies

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { appWithTranslation } from 'next-i18next';
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
-
-export default appWithTranslation(MyApp);
