c36993d0757d2335fbe9d5c2e084de08b41aa0b0
Michal Drabik <micdra7@gmail.com>
2022-02-05 18:11:45 +0100

    feat: add github and linkedin links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Hero } from 'components/hero';
 import { Layout } from 'components/layout';
+import { Socials } from 'components/socials';
 
 const App = (): JSX.Element => (
   <Layout centeredVertically>
     <Hero />
+    <Socials />
   </Layout>
 );
 
