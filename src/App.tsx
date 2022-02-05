26ae208c5b41143c32ea57128d1b281d721e323f
Michal Drabik <micdra7@gmail.com>
2022-02-05 14:47:09 +0100

    chore: setup husky and eslint sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
+import logo from './logo.svg';
+
 function App() {
+  const a = 'sdfsd';
   return (
     <div className="App">
       <header className="App-header">
